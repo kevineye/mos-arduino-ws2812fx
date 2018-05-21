c5c8c655d62d3614cf4f7fa4fda42804735f471c
Kevin Eye <eye@buffalo.edu>
2018-05-20 20:18:46 -0400

    ported a few more functions

--- src/mgos_arduino_WS2812FX.cpp
+++ src/mgos_arduino_WS2812FX.cpp
@@ -9,6 +9,19 @@ void mgos_WS2812FX_init(WS2812FX *d) {
   d->init();
 }
 
+void mgos_WS2812FX_setPixelColor(WS2812FX *d, uint16_t n, uint32_t c) {
+  if (d == nullptr) return;
+  d->setPixelColor(n, c);
+}
+void mgos_WS2812FX_setPixelColorRGB(WS2812FX *d, uint16_t n, uint8_t r, uint8_t g, uint8_t b) {
+  if (d == nullptr) return;
+  d->setPixelColor(n, r, g, b);
+}
+void mgos_WS2812FX_setPixelColorRGBW(WS2812FX *d, uint16_t n, uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
+  if (d == nullptr) return;
+  d->setPixelColor(n, r, g, b, w);
+}
+
 void mgos_WS2812FX_setBrightness(WS2812FX *d, uint8_t b) {
   if (d == nullptr) return;
   d->setBrightness(b);
@@ -17,6 +30,11 @@ void mgos_WS2812FX_setBrightness(WS2812FX *d, uint8_t b) {
 void mgos_WS2812FX_setSpeed(WS2812FX *d, uint16_t s) {
   if (d == nullptr) return;
   d->setSpeed(s);
+}
+
+void mgos_WS2812FX_setColorRGB(WS2812FX *d, uint8_t r, uint8_t g, uint8_t b) {
+  if (d == nullptr) return;
+  d->setColor(r, g, b);
 }
 
 void mgos_WS2812FX_setColor(WS2812FX *d, uint32_t c) {
@@ -32,6 +50,16 @@ void mgos_WS2812FX_setMode(WS2812FX *d, uint8_t m) {
 void mgos_WS2812FX_start(WS2812FX *d) {
   if (d == nullptr) return;
   d->start();
+}
+
+void mgos_WS2812FX_stop(WS2812FX *d) {
+  if (d == nullptr) return;
+  d->stop();
+}
+
+boolean mgos_WS2812FX_isRunning(WS2812FX *d) {
+  if (d == nullptr) return false;
+  return d->isRunning();
 }
 
 void mgos_WS2812FX_service(WS2812FX *d) {
--- src/mgos_arduino_WS2812FX.h
+++ src/mgos_arduino_WS2812FX.h
@@ -7,11 +7,20 @@ extern "C"
 
 WS2812FX *mgos_WS2812FX_create(uint16_t n, uint8_t p, neoPixelType t);
 void mgos_WS2812FX_init(WS2812FX *d);
+
+void mgos_WS2812FX_setPixelColor(WS2812FX *d, uint16_t n, uint32_t c);
+void mgos_WS2812FX_setPixelColorRGB(WS2812FX *d, uint16_t n, uint8_t r, uint8_t g, uint8_t b);
+void mgos_WS2812FX_setPixelColorRGBW(WS2812FX *d, uint16_t n, uint8_t r, uint8_t g, uint8_t b, uint8_t w);
+
 void mgos_WS2812FX_setBrightness(WS2812FX *d, uint8_t b);
 void mgos_WS2812FX_setSpeed(WS2812FX *d, uint16_t s);
+void mgos_WS2812FX_setColorRGB(WS2812FX *d, uint8_t r, uint8_t g, uint8_t b);
 void mgos_WS2812FX_setColor(WS2812FX *d, uint32_t c);
 void mgos_WS2812FX_setMode(WS2812FX *d, uint8_t m);
+
 void mgos_WS2812FX_start(WS2812FX *d);
+void mgos_WS2812FX_stop(WS2812FX *d);
+boolean mgos_WS2812FX_isRunning(WS2812FX *d);
 void mgos_WS2812FX_service(WS2812FX *d);
 
 #ifdef __cplusplus
